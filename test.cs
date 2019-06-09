4046bffe1ffe6ba84400ed0fc78e40724921f70a
AnastasiaBerdnikova <lol_bn@bk.ru>
2019-06-09 13:43:54 +0700

    Add files via upload

---
+++ test.cs
@@ -0,0 +1,23 @@
+int intk    =       10000;
+
+int Z   =  intk;
+Console.WriteLine(intk);
+Console.WriteLine("String");
+
+Console.WriteLine(intk);
+Z = 12;
+
+Console.WriteLine(Z);
+Z = intk;
+Console.WriteLine(Z);
+
+Console.ReadLine(Z);
+
+intk = 0;
+
+for (1;1;1)
+{
+	
+}
+
+while(true);
